8a0031dd5249c48bdb82960d0aafa021176749eb
Ricardo Lizarraga <rilizarraga@ucsd.edu>
2024-09-13 22:41:23 -0700

    Dataset 0, computes successfully

--- RL_projects/Final/FFT/main.c
+++ RL_projects/Final/FFT/main.c
@@ -1,9 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#include "fft.h"
 
+#include "device.h"
+#include "kernel.h"
 
+#include "matrix.h"
+#define CHECK_ERR(err, msg)                           \
+    if (err != CL_SUCCESS)                            \
+    {                                                 \
+        fprintf(stderr, "%s failed: %d\n", msg, err); \
+        exit(EXIT_FAILURE);                           \
+    }
+
+///////
+void fft(float data_re[], float data_im[], const unsigned int N);
+
+// helper functions called by the fft
+// data will first be rearranged then computed
+// an array of  {1, 2, 3, 4, 5, 6, 7, 8} will be
+// rearranged to {1, 5, 3, 7, 2, 6, 4, 8}
+void rearrange(float data_re[], float data_im[], const unsigned int N);
+void compute(float data_re[], float data_im[], const unsigned int N);
+///
+void fft(float data_re[], float data_im[], const unsigned int N)
+{
+  rearrange(data_re, data_im, N);
+  compute(data_re, data_im, N);
+}
+
+void rearrange(float data_re[], float data_im[], const unsigned int N)
+{
+  unsigned int target = 0;
+  for(unsigned int position=0; position<N;position++)
+    {
+      if(target>position) {
+	const float temp_re = data_re[target];
+	const float temp_im = data_im[target];
+	data_re[target] = data_re[position];
+	data_im[target] = data_im[position];
+	data_re[position] = temp_re;
+	data_im[position] = temp_im;
+      }
+      unsigned int mask = N;
+      while(target & (mask >>=1))
+	target &= ~mask;
+      target |= mask;
+    }
+}
+
+void compute(float data_re[], float data_im[], const unsigned int N)
+{
+  const float pi = -3.14159265358979323846;
+  
+  for(unsigned int step=1; step<N; step <<=1) {
+    const unsigned int jump = step << 1;
+    const float step_d = (float) step;
+    float twiddle_re = 1.0;
+    float twiddle_im = 0.0;
+    for(unsigned int group=0; group<step; group++)
+    {
+		for(unsigned int pair=group; pair<N; pair+=jump)
+		{
+			const unsigned int match = pair + step;
+			const float product_re = twiddle_re*data_re[match]-twiddle_im*data_im[match];
+			const float product_im = twiddle_im*data_re[match]+twiddle_re*data_im[match];
+			data_re[match] = data_re[pair]-product_re;
+			data_im[match] = data_im[pair]-product_im;
+			data_re[pair] += product_re;
+			data_im[pair] += product_im;
+		}
+	
+	// we need the factors below for the next iteration
+	// if we don't iterate then don't compute
+	if(group+1 == step)
+	  {
+	    continue;
+	  }
+
+	float angle = pi*((float) group+1)/step_d;
+	twiddle_re = cos(angle);
+	twiddle_im = sin(angle);
+      }
+  }
+}
+
+///////
 int compare_arrays(const float x[], const float y[],  const unsigned int N, const float eps);
 void print_arr(const float data[], const unsigned int N);
 void print_test_result(int tc_re, int tc_im, int tc_num);
@@ -12,72 +95,48 @@ void print_complex_data(float data_re[], float data_im[], int len);
 // We will run 4 test cases to ensure our FFT data is correct
 int main(int argc,  char **argv)
 {
-  int i; // loop iterator
-  clock_t start,  stop;
-  double cpu_time_used;
+  cl_int err;
+  const char *input0_file = argv[1], *input1_file = argv[2];
+  const char *output0_file = argv[3], *output1_file = argv[4];
+  const char *expected0_file = argv[5], *expected1_file = argv[6];
 
-  // Test Case 0 - Rearranging
-  float data0_re[8]     = {1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0};
-  float expected0_re[8] = {1.0,  5.0,  3.0,  7.0,  2.0,  6.0,  4.0,  8.0};
-  float data0_im[8]     = {1.0,  2.0,  3.0,  4.0,  5.0,  6.0,  7.0,  8.0};
-  float expected0_im[8] = {1.0,  5.0,  3.0,  7.0,  2.0,  6.0,  4.0,  8.0};
-  rearrange(data0_re,  data0_im,  8);
-  int tc0_re = compare_arrays(data0_re, expected0_re, 8, 0.01);
-  int tc0_im = compare_arrays(data0_im, expected0_im, 8, 0.01);
-  print_test_result(tc0_re, tc0_im, 0);
+  Matrix input0; //input0 Re
+  Matrix input1; //input0 I
+  Matrix output0; //output0 Re
+  Matrix output1; //output1 Im
+  Matrix expected0; //expected0 Re
+  Matrix expected1; //expected1 Im
+  err = LoadMatrix(input0_file, &input0);
+  CHECK_ERR(err, "LoadMatrix");
+  err = LoadMatrix(input1_file, &input1);
+  CHECK_ERR(err, "LoadMatrix");
+  //expected
+  err = LoadMatrix(expected0_file, &expected0);
+  CHECK_ERR(err, "LoadMatrix");
+  err = LoadMatrix(expected1_file, &expected1);
+  CHECK_ERR(err, "LoadMatrix");
+
+  unsigned int N = input0.shape[0];
+  printf("---Input0 & 1:\n");
+  for(unsigned int n=0;n<N;n++)
+    printf("%.2f + j(%.2f)\n", (float) input0.data[n],  (float) input1.data[n]);
+  
+  printf("---Expected 0 & 1:\n");
+  for(unsigned int n=0;n<N;n++)
+    printf("%.2f + j(%.2f)\n", (float) expected0.data[n],  (float) expected1.data[n]);
+  
+  printf("---Input Data (Files: input0.raw & input1.raw), N = %d\n", N);
+  print_complex_data(input0.data, input1.data, N);
 
   // Test Case 1
-  float data1_re[8]     = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
-  float data1_im[8]     = {7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0.0};
-  float expected1_re[8] = {28.0, 5.656, 0.0, -2.343, -4.0, -5.656, -8.0, -13.656};
-  float expected1_im[8] = {28.0, 13.656, 8.0, 5.656, 4.0, 2.343, 0.0, -5.656};
-  printf("---Time Domain:\n");print_complex_data(data1_re, data1_im, 8);
-  fft(data1_re, data1_im, 8);
-  printf("---Frequency Domain:\n");print_complex_data(data1_re, data1_im, 8);
+  fft(input0.data, input1.data, N); //fft(data1_re, data1_im, 8);
+  
+  printf("---Frequency Domain:\n");
+  print_complex_data(input0.data, input1.data, N); //print_complex_data(data1_re, data1_im, N);
 
-  int tc1_re = compare_arrays(data1_re, expected1_re, 8, 0.01);
-  int tc1_im = compare_arrays(data1_im, expected1_im, 8, 0.01);
-  print_test_result(tc1_re, tc1_im, 1);
-
-  // Test Case 2
-  float data2_re[8]     = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-  float data2_im[8]     = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-  float expected2_re[8] = {8.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  float expected2_im[8] = {8.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  fft(data2_re, data2_im, 8);
-  int tc2_re = compare_arrays(data2_re, expected2_re, 8, 0.01);
-  int tc2_im = compare_arrays(data2_im, expected2_im, 8, 0.01);
-  print_test_result(tc2_re, tc2_im, 2);
-
-  // Test Case 3
-  float data3_re[8]     = { 1.0, -1.0,  1.0, -1.0,  1.0, -1.0,  1.0, -1.0};
-  float data3_im[8]     = {-1.0,  1.0, -1.0,  1.0, -1.0,  1.0, -1.0,  1.0};
-  float expected3_re[8] = {0.0, 0.0, 0.0, 0.0,  8.0, 0.0, 0.0, 0.0};
-  float expected3_im[8] = {0.0, 0.0, 0.0, 0.0, -8.0, 0.0, 0.0, 0.0};
-  fft(data3_re, data3_im, 8);
-  int tc3_re = compare_arrays(data3_re, expected3_re, 8, 0.01);
-  int tc3_im = compare_arrays(data3_im, expected3_im, 8, 0.01);
-  print_test_result(tc3_re, tc3_im, 3);
-
-  // Test Case 4
-  float data4_re[4]     = {1.0, 2.0, 3.0, 4.0};
-  float data4_im[4]     = {0.0, 0.0, 0.0, 0.0};
-  float expected4_re[4] = {10.0, -2.0, -2.0, -2.0};
-  float expected4_im[4] = {0.0, 2.0, 0.0, -2.0};
-  fft(data4_re, data4_im, 4);
-  int tc4_re = compare_arrays(data4_re, expected4_re, 4, 0.01);
-  int tc4_im = compare_arrays(data4_im, expected4_im, 4, 0.01);
-  print_test_result(tc4_re, tc4_im, 4);
-
-  // Test Case 5
-  float data5_re[128];
-  float data5_im[128];
-
-  start = clock();
-  for(i=0;i<100000;i++) fft(data5_re, data5_im, 128);
-  stop = clock();
-  cpu_time_used = ((double) (stop - start)) / CLOCKS_PER_SEC;
-  printf("Average time per fft %fms\n", cpu_time_used/1000);
+  // Check the result of the matrix multiply
+  CheckMatrix(&input0, &expected0);
+  CheckMatrix(&input1, &expected1);
 }
 
 void print_complex_data(float data_re[], float data_im[], int len)
